242d1ddc1abc16390bcd83f5fb616b25bc9b7f5d
manishwaran <manishwaran.np@gmail.com>
2017-04-15 11:23:01 +0530

    removing logs.

--- src/tree/bk-tree/search/index.js
+++ src/tree/bk-tree/search/index.js
@@ -9,7 +9,6 @@ class BKTreeSearch extends BKTree {
   isTolerable(value, distance, tolerance, word, word1) {
     const negativeTolerance = value - tolerance
     const positiveTolerance = value + tolerance
-    console.log(negativeTolerance, positiveTolerance, distance, word, word1);
     if (distance >= negativeTolerance && distance <= positiveTolerance) {
       return true;
     }
@@ -18,12 +17,10 @@ class BKTreeSearch extends BKTree {
 
   searchData(word, result, tolerance, tree = this.tree) {
     const diff = this.calculateDistance(tree.data, word)
-    console.log('diff', diff, tree.data);
     const distances = this.getDistances(tree)
     distances.forEach((distance) => {
       if(this.isTolerable(diff, distance, tolerance, tree.data, tree[distance].data)) {
         result.push(this.tree[distance].data)
-        console.log('next', this.tree[distance].data);
         this.searchData(word, result, tolerance, tree[distance])
       }
     })
@@ -39,5 +36,4 @@ class BKTreeSearch extends BKTree {
 const bkTree = new BKTreeSearch()
 const data = ['help', 'hell', 'helps', 'hello', 'shell', 'helper', 'loops', 'troop']
 data.forEach(word => bkTree.insert(word))
-console.log(JSON.stringify(bkTree, undefined, 3));
 console.log(bkTree.search('oops'))
